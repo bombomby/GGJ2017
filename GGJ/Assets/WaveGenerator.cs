4252ce258861ad0841c595dba611138018be6c6f
Benoit Chauvin <benoit.chauvin@gmail.com>
2017-01-20 23:57:44 +0000

    waves_xxx functions

--- GGJ/Assets/WaveGenerator.cs
+++ GGJ/Assets/WaveGenerator.cs
@@ -9,7 +9,34 @@ public class WaveGenerator : MonoBehaviour {
         return Mathf.Sin(20.0f * Mathf.PI * x);
     }
 
+<<<<<<< .mine
+    float WaveFunction_001(float x)
+    {
+        return (float)(-System.Math.Cos(3 * x * System.Math.PI) * System.Math.Sin(x * System.Math.PI));
+    }
+
+    float WaveFunction_002(float x)
+    {
+        return (float)(System.Math.Sin(System.Math.PI * x * 10) * System.Math.Sin(x * System.Math.PI) * System.Math.Sin(System.Math.PI * (x - 1.0)));
+    }
+
+    float WaveFunction_003(float x)
+    {
+        return (float)(System.Math.Sin(x * System.Math.PI) / 7);
+    }
+
+    float WaveFunction_004(float x)
+    {
+        return (float)(System.Math.Cos(2 * System.Math.PI * x));
+    }
+
+    Vector2[] GenerateWaves(int count)
+||||||| .r8
+
+    Vector2[] GenerateWaves(int count)
+=======
     Vector2[] GenerateWaves(int count, Vector2 scale)
+>>>>>>> .r11
     {
         Vector2[] waves = new Vector2[count];
 
