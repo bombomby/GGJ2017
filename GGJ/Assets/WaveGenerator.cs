b22ddc39553a5d9124274e4d521bdc51c4c87822
Vadim Slyusarev <nb_grif@mail.ru>
2017-01-21 01:41:04 +0000

    WaveGen

--- GGJ/Assets/WaveGenerator.cs
+++ GGJ/Assets/WaveGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,19 +24,6 @@ public class WaveGenerator : MonoBehaviour {
         return Mathf.Sin(20.0f * Mathf.PI * x);
     }
 
-    Vector2[] GenerateWaves(int count, Vector2 scale)
-    {
-        Vector2[] waves = new Vector2[count];
-
-        for (int i = 0; i < count; ++i)
-        {
-            float x = (float)i / count;
-            waves[i] = new Vector2(scale.x * x, scale.y * WaveFunction(x));
-        }
-
-        return waves;
-    }
-
     EdgeCollider2D waveCollider;
 
     public Vector2 WaveScale = new Vector2(1, 1);
@@ -43,17 +31,27 @@ public class WaveGenerator : MonoBehaviour {
 
     List<Vector2> points = new List<Vector2>();
 
+    WaveCollection WaveCollection = new WaveCollection();
+
     // Use this for initialization
     void Awake () {
         waveCollider = GetComponent<EdgeCollider2D>();
+
+        AddWave(WaveCollection.Waves[0]);
+
+        for (int i = 0; i < 1024; ++i)
+        {
+            UpdateWave(1 / 60.0f);
+        }
         //waveCollider.points = GenerateWaves(1024, WaveScale);
     }
 
     public float Height = 0.0f;
     public float Length = 0.0f;
 
-    void UpdateWave(float step)
+    void UpdateWave(float time)
     {
+        float step = time / WaveScale.x;
         float value = 0.0f;
 
         waves.ForEach(wave =>
@@ -62,18 +60,17 @@ public class WaveGenerator : MonoBehaviour {
             value += wave.Function(wave.X);
         });
 
-        Height += value;
+        Height = value * WaveScale.y;
+        Length += time * Speed;
+
+        points.Add(new Vector2(-Length, Height));
     }
-
-
 
 
 	// Update is called once per frame
 	void FixedUpdate () {
-        UpdateWave(Time.deltaTime);
-        Length += Time.deltaTime;
 
-        points.Add(new Vector2(-Length, Height));
+        
         waveCollider.points = points.ToArray();
 
         transform.position = transform.position + new Vector3(Time.deltaTime * Speed, 0.0f, 0.0f);
