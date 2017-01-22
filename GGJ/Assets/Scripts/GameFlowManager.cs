aee9fcec4f33a4204d6465d7dcdae28e3d9e138e
Vadim Slyusarev <nb_grif@mail.ru>
2017-01-22 01:03:55 +0000

    Fixed blocks

--- GGJ/Assets/Scripts/GameFlowManager.cs
+++ GGJ/Assets/Scripts/GameFlowManager.cs
@@ -32,7 +32,7 @@ public class GameFlowManager : MonoBehaviour
 
 	public float m_score = 0.0f;
 
-    public float Speed = 1.0f;
+    public float Speed = -1.0f;
 
 	// Use this for initialization
 	void Start ()
